a77d0cdd475581183e4dd29efa58bed1e8ba8867
sidnewby <snewby@platinumids.com>
2024-06-24 13:24:37 -0500

    ss
    Signed-off-by: sidnewby <snewby@platinumids.com>

--- game.js
+++ game.js
@@ -2,12 +2,6 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 }
-        }
-    },
     scene: {
         preload: preload,
         create: create,
@@ -17,58 +11,32 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-let snake;
-let food;
+let face;
 let cursors;
-let pestControl;
 
 function preload() {
     this.load.image('face', 'assets/your-face.png');
-    this.load.image('food', 'assets/food.png');
-    this.load.image('pest', 'assets/pest.png');
 }
 
 function create() {
-    snake = this.physics.add.image(400, 300, 'face').setScale(0.1);
-    snake.setCollideWorldBounds(true);
-
-    food = this.physics.add.image(Phaser.Math.Between(50, 750), Phaser.Math.Between(50, 550), 'food').setScale(0.05);
-
-    pestControl = this.physics.add.image(0, 0, 'pest').setScale(0.1);
-
+    face = this.add.image(400, 300, 'face');
+    face.setScale(0.2);  // Adjust this value to resize your face image
+    
     cursors = this.input.keyboard.createCursorKeys();
-
-    this.physics.add.overlap(snake, food, eatFood, null, this);
-    this.physics.add.overlap(snake, pestControl, gameOver, null, this);
+    
+    console.log('Game created');  // Debug log
 }
 
 function update() {
     if (cursors.left.isDown) {
-        snake.setVelocityX(-160);
+        face.x -= 4;
     } else if (cursors.right.isDown) {
-        snake.setVelocityX(160);
-    } else {
-        snake.setVelocityX(0);
+        face.x += 4;
     }
 
     if (cursors.up.isDown) {
-        snake.setVelocityY(-160);
+        face.y -= 4;
     } else if (cursors.down.isDown) {
-        snake.setVelocityY(160);
-    } else {
-        snake.setVelocityY(0);
+        face.y += 4;
     }
-
-    // Move pest control towards snake
-    this.physics.moveToObject(pestControl, snake, 100);
-}
-
-function eatFood(snake, food) {
-    food.setPosition(Phaser.Math.Between(50, 750), Phaser.Math.Between(50, 550));
-}
-
-function gameOver() {
-    this.physics.pause();
-    snake.setTint(0xff0000);
-    this.add.text(400, 300, 'Game Over', { fontSize: '64px', fill: '#000' }).setOrigin(0.5);
 }
