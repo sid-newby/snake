048e602b95f6580224ba63c6e59f15669351759d
sidnewby <snewby@platinumids.com>
2024-06-24 22:42:27 -0500

    i heard i could call a hotline for help.
    Signed-off-by: sidnewby <snewby@platinumids.com>

--- game.js
+++ game.js
@@ -13,124 +13,11 @@ const config = {
         debug: false
       }
     },
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
-    },
+    scene: MyScene,
     audio: {
       disableWebAudio: true
     }
 };
-
-const game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image('face', 'assets/your-face.png');
-    this.load.image('body', 'assets/snake-body.png');
-    this.load.image('pest', 'assets/pest.png');
-    this.load.image('background', 'assets/background.png');
-    this.load.image('unicorn', 'assets/unicorn.png');
-    this.load.image('bumblebee', 'assets/bumblebee.png');
-    this.load.image('cover', 'assets/cover.png');
-
-    this.load.audio('move', 'assets/move.mp3');
-    this.load.audio('collect', 'assets/collect.mp3');
-    this.load.audio('gameover', 'assets/gameover.mp3');
-    this.load.audio('bgmusic', 'assets/background-music.mp3');
-}
-
-function create() {
-    this.add.image(800, 450, 'background').setDisplaySize(1600, 900);
-
-    this.face = this.physics.add.image(800, 450, 'face').setScale(0.5);
-    this.face.setDepth(1);
-
-    this.pestControl = this.physics.add.image(100, 100, 'pest').setScale(this.face.scale * 1.5);
-    this.unicorn = this.physics.add.image(1500, 800, 'unicorn').setScale(this.face.scale * 1.75);
-    this.bumblebee = this.physics.add.image(200, 100, 'bumblebee').setScale(this.face.scale * 0.7);
-
-    // Hide game objects initially
-    [this.face, this.pestControl, this.unicorn, this.bumblebee].forEach(obj => obj.setVisible(false));
-
-    this.cursors = this.input.keyboard.createCursorKeys();
-    this.touchPointer = this.input.activePointer;
-
-    this.moveSound = this.sound.add('move');
-    this.collectSound = this.sound.add('collect');
-    this.gameOverSound = this.sound.add('gameover');
-    this.backgroundMusic = this.sound.add('bgmusic', { loop: true, volume: 0.5 });
-
-    this.coverScreen = this.add.image(800, 450, 'cover');
-    this.coverScreen.setDisplaySize(1600, 900);
-    this.coverScreen.setDepth(10);
-
-    this.startText = this.add.text(400, 225, 'you cant read.', { 
-        fontSize: '64px', 
-        fill: '#ffffff',
-        stroke: '#0bb6dd',
-        strokeThickness: 6
-    });
-    this.startText.setOrigin(0.5);
-    this.startText.setDepth(11);
-
-    this.coverScreen.setInteractive();
-    this.coverScreen.on('pointerdown', this.revealGameBoard, this);
-
-    this.gameOver = false;
-    this.gameStarted = false;
-}
-
-function update() {
-    if (!this.gameStarted || this.gameOver) return;
-
-    let velocityX = 0;
-    let velocityY = 0;
-
-    if (this.cursors.left.isDown || (this.touchPointer.isDown && this.touchPointer.x < config.scale.width / 3)) {
-        velocityX = -4;
-    } else if (this.cursors.right.isDown || (this.touchPointer.isDown && this.touchPointer.x > config.scale.width * 2 / 3)) {
-        velocityX = 4;
-    }
-    if (this.cursors.up.isDown || (this.touchPointer.isDown && this.touchPointer.y < config.scale.height / 3)) {
-        velocityY = -4;
-    } else if (this.cursors.down.isDown || (this.touchPointer.isDown && this.touchPointer.y > config.scale.height * 2 / 3)) {
-        velocityY = 4;
-    }
-
-    if (velocityX !== 0 || velocityY !== 0) {
-        if (!this.moveSound.isPlaying) this.moveSound.play();
-    }
-
-    this.face.setVelocity(velocityX * 50, velocityY * 50);
-
-    this.moveEnemyTowards(this.pestControl, 2);
-    this.moveEnemyTowards(this.unicorn, 1);
-    this.moveEnemyTowards(this.bumblebee, 1.5);
-
-    this.physics.world.collide(this.face, [this.pestControl, this.unicorn, this.bumblebee], this.handleCollision, this.checkCollisionDistance, this);
-}
-
-revealGameBoard() {
-    this.tweens.add({
-        targets: [this.coverScreen, this.startText],
-        alpha: 0,
-        duration: 1000,
-        onComplete: () => {
-            this.coverScreen.destroy();
-            this.startText.destroy();
-            this.startGame();
-        }
-    });
-}
-
-startGame() {
-    this.gameStarted = true;
-    this.backgroundMusic.play();
-
-    // Make game objects visible
-    [this.face, this.pestControl, this.unicorn, this.bumblebee].forEach(obj => obj.setVisible(true));
-}
 
 class MyScene extends Phaser.Scene {
     constructor() {
@@ -138,68 +25,129 @@ class MyScene extends Phaser.Scene {
     }
 
     preload() {
-        // Your preload code
+        this.load.image('face', 'assets/your-face.png');
+        this.load.image('body', 'assets/snake-body.png');
+        this.load.image('pest', 'assets/pest.png');
+        this.load.image('background', 'assets/background.png');
+        this.load.image('unicorn', 'assets/unicorn.png');
+        this.load.image('bumblebee', 'assets/bumblebee.png');
+        this.load.image('cover', 'assets/cover.png');
+
+        this.load.audio('move', 'assets/move.mp3');
+        this.load.audio('collect', 'assets/collect.mp3');
+        this.load.audio('gameover', 'assets/gameover.mp3');
+        this.load.audio('bgmusic', 'assets/background-music.mp3');
     }
 
     create() {
-        // Your create code (the function you shared)
-    }
+        this.add.image(800, 450, 'background').setDisplaySize(1600, 900);
 
-    revealGameBoard() {
-        // The revealGameBoard function from step 1
-    }
+        this.face = this.physics.add.image(800, 450, 'face').setScale(0.5);
+        this.face.setDepth(1);
 
-    startGame() {
-        // The startGame function from step 2
+        this.pestControl = this.physics.add.image(100, 100, 'pest').setScale(this.face.scale * 1.5);
+        this.unicorn = this.physics.add.image(1500, 800, 'unicorn').setScale(this.face.scale * 1.75);
+        this.bumblebee = this.physics.add.image(200, 100, 'bumblebee').setScale(this.face.scale * 0.7);
+
+        // Hide game objects initially
+        [this.face, this.pestControl, this.unicorn, this.bumblebee].forEach(obj => obj.setVisible(false));
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.touchPointer = this.input.activePointer;
+
+        this.moveSound = this.sound.add('move');
+        this.collectSound = this.sound.add('collect');
+        this.gameOverSound = this.sound.add('gameover');
+        this.backgroundMusic = this.sound.add('bgmusic', { loop: true, volume: 0.5 });
+
+        this.coverScreen = this.add.image(800, 450, 'cover');
+        this.coverScreen.setDisplaySize(1600, 900);
+        this.coverScreen.setDepth(10);
+
+        this.startText = this.add.text(400, 225, 'you cant read.', { 
+            fontSize: '64px', 
+            fill: '#ffffff',
+            stroke: '#0bb6dd',
+            strokeThickness: 6
+        });
+        this.startText.setOrigin(0.5);
+        this.startText.setDepth(11);
+
+        this.coverScreen.setInteractive();
+        this.coverScreen.on('pointerdown', this.revealGameBoard, this);
+
+        this.gameOver = false;
+        this.gameStarted = false;
     }
 
     update() {
-        // Your update code
+        if (!this.gameStarted || this.gameOver) return;
+
+        let velocityX = 0;
+        let velocityY = 0;
+
+        if (this.cursors.left.isDown || (this.touchPointer.isDown && this.touchPointer.x < config.scale.width / 3)) {
+            velocityX = -4;
+        } else if (this.cursors.right.isDown || (this.touchPointer.isDown && this.touchPointer.x > config.scale.width * 2 / 3)) {
+            velocityX = 4;
+        }
+        if (this.cursors.up.isDown || (this.touchPointer.isDown && this.touchPointer.y < config.scale.height / 3)) {
+            velocityY = -4;
+        } else if (this.cursors.down.isDown || (this.touchPointer.isDown && this.touchPointer.y > config.scale.height * 2 / 3)) {
+            velocityY = 4;
+        }
+
+        if (velocityX !== 0 || velocityY !== 0) {
+            if (!this.moveSound.isPlaying) this.moveSound.play();
+        }
+
+        this.face.setVelocity(velocityX * 50, velocityY * 50);
+
+        this.moveEnemyTowards(this.pestControl, 2);
+        this.moveEnemyTowards(this.unicorn, 1);
+        this.moveEnemyTowards(this.bumblebee, 1.5);
+
+        this.physics.world.collide(this.face, [this.pestControl, this.unicorn, this.bumblebee], this.handleCollision, this.checkCollisionDistance, this);
+    }
+
+    revealGameBoard() {
+        this.tweens.add({
+            targets: [this.coverScreen, this.startText],
+            alpha: 0,
+            duration: 1000,
+            onComplete: () => {
+                this.coverScreen.destroy();
+                this.startText.destroy();
+                this.startGame();
+            }
+        });
+    }
+
+    startGame() {
+        this.gameStarted = true;
+        this.backgroundMusic.play();
+
+        // Make game objects visible
+        [this.face, this.pestControl, this.unicorn, this.bumblebee].forEach(obj => obj.setVisible(true));
+    }
+
+    moveEnemyTowards(enemy, speed) {
+        this.physics.moveTo(enemy, this.face.x, this.face.y, speed * 50);
+    }
+
+    checkCollisionDistance(player, enemy) {
+        const maxCollisionDistance = 50; // pixels
+        const distance = Phaser.Math.Distance.Between(player.x, player.y, enemy.x, enemy.y);
+        return distance < maxCollisionDistance;
+    }
+
+    handleCollision() {
+        this.gameOver = true;
+        this.backgroundMusic.stop();
+        this.gameOverSound.play();
+        this.add.text(800, 450, 'Game Over!', { fontSize: '64px', fill: '#fff' }).setOrigin(0.5);
+        this.scene.pause();
     }
 }
 
-moveEnemyTowards(enemy, speed) {
-    this.physics.moveTo(enemy, this.face.x, this.face.y, speed * 50);
-}
-
-const MyScene = new Phaser.Class({
-    Extends: Phaser.Scene,
-
-    initialize: function MyScene() {
-        Phaser.Scene.call(this, { key: 'MyScene' });
-    },
-
-    preload: function() {
-        // Your preload code
-    },
-
-    create: function() {
-        // Your create code (the function you shared)
-    },
-
-    revealGameBoard: function() {
-        // The revealGameBoard function from step 1
-    },
-
-    startGame: function() {
-        // The startGame function from step 2
-    },
-
-    update: function() {
-        // Your update code
-    }
-});
-
-function checkCollisionDistance(player, enemy) {
-    const maxCollisionDistance = 50; // pixels
-    const distance = Phaser.Math.Distance.Between(player.x, player.y, enemy.x, enemy.y);
-    return distance < maxCollisionDistance;
-}
-
-function handleCollision() {
-    this.gameOver = true;
-    this.backgroundMusic.stop();
-    this.gameOverSound.play();
-    this.add.text(800, 450, 'Game Over!', { fontSize: '64px', fill: '#fff' }).setOrigin(0.5);
-    this.scene.pause();
-}
+const game = new Phaser.Game(config);
